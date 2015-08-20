6fd9a8b7e8f071a92a154f7f13cafe268d577202
dodola <doraemon483@hotmail.com>
2015-08-20 22:53:14 +0800

    Initial Commit

---
+++ Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by 苟颖琦 on 15/8/20.
+//  Copyright (c) 2015年 dgz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
